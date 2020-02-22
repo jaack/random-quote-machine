fc7e91852774eb0754a49dbfc51bea0f311c93da
Jack <chugga151@gmail.com>
2020-02-22 14:27:55 +0000

    Added initial build

---
+++ precache-manifest.67c8c8e95ffbe2436171d21fed19bd4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a553346c9347357b47aeeb654237991",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "9215f93f9c6fe5643372",
+    "url": "/random-quote-machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbe3a8effd7fe5ad659d",
+    "url": "/random-quote-machine/static/js/2.3d314535.chunk.js"
+  },
+  {
+    "revision": "69a5d852ba430bd4deea93fd80a89a51",
+    "url": "/random-quote-machine/static/js/2.3d314535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9215f93f9c6fe5643372",
+    "url": "/random-quote-machine/static/js/main.889844b5.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
